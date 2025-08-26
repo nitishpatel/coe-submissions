134988d32aebfa9f02b2f3dc55dd272f1ba82683
Nitish Patel <nitishp74@gmail.com>
2025-08-26 12:16:55 +0530

    chore: testing environment and prettier setup

--- react-state-mangement/eslint.config.js
+++ react-state-mangement/eslint.config.js
@@ -1,7 +1,11 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-config-prettier'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
@@ -12,12 +16,31 @@ export default tseslint.config([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
+      jsxA11y.configs.recommended,
       reactRefresh.configs.vite,
+      prettier
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tseslint.parser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off'
+    },
+  },
+  {
+    files: ['**/*.{test,spec}.{ts,tsx,js,jsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
 ])
