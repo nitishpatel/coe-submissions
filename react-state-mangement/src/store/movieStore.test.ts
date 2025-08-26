8f8f6d596452a0aea71e233befe2fa26dd0e1dae
Nitish Patel <nitishp74@gmail.com>
2025-08-26 13:07:33 +0530

    test: adding a movie

--- react-state-mangement/src/store/movieStore.test.ts
+++ react-state-mangement/src/store/movieStore.test.ts
@@ -15,5 +15,15 @@ describe('movie store', () => {
     expect(movieState.movies).toEqual([]);
     expect(movieState.filter).toBe("all");
     expect(movieState.loading).toBe(false);
-  })
+  });
+
+  it("can add a movie",()=>{
+    const movieStore = useMovieStore.getState();
+    movieStore.addMovie?.("ABCD","At Home");
+    const {movies} = useMovieStore.getState();
+    expect(movies.length).toBe(1);
+    expect(movies[0].title).toBe("ABCD");
+    expect(movies[0].note).toBe("At Home");
+    expect(movies[0].watched).toBe(false);
+  });
 });
