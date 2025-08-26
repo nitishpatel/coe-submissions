ab2b623976104ce2d89e010a558eb2dd7dd90945
Nitish Patel <nitishp74@gmail.com>
2025-08-26 13:29:39 +0530

    feat: implemented add movile method

--- react-state-mangement/src/store/movieStore.ts
+++ react-state-mangement/src/store/movieStore.ts
@@ -8,12 +8,28 @@ type State = {
   loading: boolean;
 };
 
-export const useMovieStore = create<State>()(
-  persist<State>(
-    (_set, _get): State => ({
-      movies: [] as Movie[],
+type Actions = {
+  addMovie: (title: string, note?: string) => void;
+};
+
+export const useMovieStore = create<State & Actions>()(
+  persist<State & Actions>(
+    (set, _get): State & Actions => ({
+      movies: [],
       filter: 'all' as Filter,
       loading: false,
+
+      addMovie: (title, note) => {
+        const id = crypto.randomUUID();
+        const createdAt = new Date().getTime();
+        const watched = false;
+        set((s) => ({
+          movies: [
+            { id: id, title: title, note: note, watched: watched, createdAt: createdAt },
+            ...s.movies
+          ]
+        }))
+      },
     }),
     { name: 'movie-store' }
   )
