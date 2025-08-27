8a31c2ec8c7b5dba7b550492d46bb1279a0c38a6
Nitish Patel <nitishp74@gmail.com>
2025-08-27 16:15:26 +0530

    test: add validation test for empty item name

--- user-registration/test_flask.py
+++ user-registration/test_flask.py
@@ -15,3 +15,12 @@ def test_basic_pydantic_success_validation(client):
   response = client.post("/items/", json={"name": "Item", "price": 10.0})
   assert response.status_code == 200
   assert response.json == {"name": "Item", "price": 10.0}
+
+
+def test_item_name_validation(client):
+    res = client.post("/items/", json={"name": "", "price": 10.0})
+    print(res.json)
+    assert res.status_code == 400
+    assert res.json["validation_error"]["body_params"][0]["loc"] == ["name"]
+    assert res.json["validation_error"]["body_params"][0]["msg"] == "String should have at least 1 character"
+    assert res.json["validation_error"]["body_params"][0]["type"] == "string_too_short"
