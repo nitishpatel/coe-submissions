3bdf5d35518b2634fb9957d95a0bd95ea1f7fcec
Nitish Patel <nitishp74@gmail.com>
2025-08-27 16:18:00 +0530

    test: enhance item name validation tests for minimum and maximum length

--- user-registration/test_flask.py
+++ user-registration/test_flask.py
@@ -19,8 +19,24 @@ def test_basic_pydantic_success_validation(client):
 
 def test_item_name_validation(client):
     res = client.post("/items/", json={"name": "", "price": 10.0})
-    print(res.json)
     assert res.status_code == 400
     assert res.json["validation_error"]["body_params"][0]["loc"] == ["name"]
     assert res.json["validation_error"]["body_params"][0]["msg"] == "String should have at least 1 character"
     assert res.json["validation_error"]["body_params"][0]["type"] == "string_too_short"
+
+def test_min_max_length_name_validation(client):
+    res = client.post("/items/", json={"name": "", "price": 10.0})
+    assert res.status_code == 400
+    assert res.json["validation_error"]["body_params"][0]["loc"] == ["name"]
+    assert res.json["validation_error"]["body_params"][0]["msg"] == "String should have at least 1 character"
+    assert res.json["validation_error"]["body_params"][0]["type"] == "string_too_short"
+
+    res = client.post("/items/", json={"name": "LongEnough", "price": 10.0})
+    assert res.status_code == 200
+    assert res.json == {"name": "LongEnough", "price": 10.0}
+
+    res = client.post("/items/", json={"name": "A" * 101, "price": 10.0})
+    assert res.status_code == 400
+    assert res.json["validation_error"]["body_params"][0]["loc"] == ["name"]
+    assert res.json["validation_error"]["body_params"][0]["msg"] == "String should have at most 100 characters"
+    assert res.json["validation_error"]["body_params"][0]["type"] == "string_too_long"
