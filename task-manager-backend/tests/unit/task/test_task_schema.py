b0707e6659bb5a75a99c5e6f4acb26dcd3cdf590
Nitish Patel <nitishp74@gmail.com>
2025-08-28 17:44:16 +0530

    test: invalid task status

--- task-manager-backend/tests/unit/task/test_task_schema.py
+++ task-manager-backend/tests/unit/task/test_task_schema.py
@@ -16,3 +16,7 @@ def test_task_title_min_and_max_length():
         TaskCreate(title="A", description="Invalid description")
     with pytest.raises(Exception):
         TaskCreate(title="A" * 201, description="Invalid description")
+
+def test_task_create_with_invalid_status():
+    with pytest.raises(Exception):
+        TaskCreate(title="Valid Title", status="INVALID_STATUS")
