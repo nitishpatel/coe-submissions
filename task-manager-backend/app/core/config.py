1132a95779443ea2393677612e50c0a8f2b4062b
Nitish Patel <nitishp74@gmail.com>
2025-08-28 12:20:11 +0530

    feat: config setup along with dummy fastapi app

---
+++ task-manager-backend/app/core/config.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+
+
+class Config:
+    _instance = None
+
+    def __init__(self):
+        load_dotenv()
+        self.postgres_user = os.getenv("POSTGRES_USER")
+        self.postgres_password = os.getenv("POSTGRES_PASSWORD")
+        self.postgres_db = os.getenv("POSTGRES_DB")
+        self.postgres_port = os.getenv("POSTGRES_PORT")
+        self.postgres_url = f"postgresql://{self.postgres_user}:{self.postgres_password}@localhost:{self.postgres_port}/{self.postgres_db}"
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+
+config = Config.get_instance()
