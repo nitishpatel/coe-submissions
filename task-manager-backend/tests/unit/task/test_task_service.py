34beb6d02743a51ec6e7b62595e371efe3be08c2
Nitish Patel <nitishp74@gmail.com>
2025-08-28 16:37:43 +0530

    test: added test for task delete for task service

--- task-manager-backend/tests/unit/task/test_task_service.py
+++ task-manager-backend/tests/unit/task/test_task_service.py
@@ -1,5 +1,5 @@
 from app.services.task import TaskService
-from app.schemas.task import TaskCreate
+from app.schemas.task import TaskCreate,TaskUpdate
 
 def test_service_create_and_get(db):
     svc = TaskService()
@@ -8,3 +8,19 @@ def test_service_create_and_get(db):
     assert created.title == "Day 3 - Task"
     got = svc.get(db, created.id)
     assert got is not None and got.title == "Day 3 - Task"
+
+def test_service_task_update(db):
+    svc = TaskService()
+    created = svc.create(db, TaskCreate(title="Day 3 - Task"))
+    assert created.id is not None
+    assert created.title == "Day 3 - Task"
+    updated = svc.update(db, created.id, TaskUpdate(title="Day 3 - Task Updated"))
+    assert updated is not None
+    assert updated.title == "Day 3 - Task Updated"
+
+def test_service_task_delete(db):
+    svc = TaskService()
+    created = svc.create(db, TaskCreate(title="Day 3 - Task"))
+    deleted = svc.delete(db, created.id)   # pass id
+    assert deleted is True
+    assert svc.get(db, created.id) is None
