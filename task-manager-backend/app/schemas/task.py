4c318ea2ed688b3fc146b21e8ff1fd9c9e68a745
Nitish Patel <nitishp74@gmail.com>
2025-08-28 15:27:24 +0530

    feat: added pydantic schema for Task Model

---
+++ task-manager-backend/app/schemas/task.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field,ConfigDict
+from datetime import datetime
+from app.models.task import TaskStatus
+from enum import Enum
+
+class TaskBase(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
+    description: str | None = None
+    status: TaskStatus = TaskStatus.TODO
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(TaskBase):
+    title: str | None = Field(default=None, min_length=1, max_length=200)
+    description: str | None = None
+    status: TaskStatus | None = None
+
+class TaskRead(TaskBase):
+    id: str
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
