997b8892fcaadc9a6325d1c7d9fd53728a1ad4d9
Nitish Patel <nitish@incubyte.co>
2025-08-29 12:23:00 +0530

    feat: fixed full name non mandatory in user signup request

--- task-manager-backend/app/schemas/user.py
+++ task-manager-backend/app/schemas/user.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel,EmailStr,ConfigDict
 
 class UserSignUpRequest(BaseModel):
     email: EmailStr
-    full_name: str
+    full_name: str | None = None
     password: str
