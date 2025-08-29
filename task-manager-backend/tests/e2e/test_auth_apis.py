80965158760d45322a9c43a1baee10b34842b11e
Nitish Patel <nitish@incubyte.co>
2025-08-29 16:43:34 +0530

    test: duplicate email test

--- task-manager-backend/tests/e2e/test_auth_apis.py
+++ task-manager-backend/tests/e2e/test_auth_apis.py
@@ -13,3 +13,10 @@ def test_user_signup_api(client, user_signup):
     assert response.status_code == 201
     assert UserRead.model_validate(response.json())
     assert response.json()["email"] == "test1@example.in"
+
+def test_should_not_allow_duplicate_email_signup(client, user_signup):
+    response1 = user_signup(email="test@example.com")
+    assert response1.status_code == 201
+    response2 = user_signup(email="test@example.com")
+    assert response2.status_code == 400
+    assert response2.json()["detail"] == "Email already exists"
