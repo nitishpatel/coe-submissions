078297970918d10309d3b933a9512fe63ef9cc48
Nitish Patel <nitish@incubyte.co>
2025-08-29 12:16:47 +0530

    test: added test for user sign up request schema invalid email test

--- task-manager-backend/tests/unit/user/test_user_schema.py
+++ task-manager-backend/tests/unit/user/test_user_schema.py
@@ -12,3 +12,11 @@ def test_successful_user_signup():
     assert user.password == "StrongPassword123!"
 
 
+def test_invalid_email_user_signup():
+    with pytest.raises(ValueError) as exc_info:
+        UserSignUpRequest(
+            email="invalid-email",
+            full_name="Test User",
+            password="StrongPassword123!"
+        )
+    assert "value is not a valid email address" in str(exc_info.value)
