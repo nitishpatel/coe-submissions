14b3c7bba65ed701258bee6fc689d68068d72881
Nitish Patel <nitish@incubyte.co>
2025-08-29 17:45:06 +0530

    test: added login success testcase

--- task-manager-backend/tests/e2e/test_auth_apis.py
+++ task-manager-backend/tests/e2e/test_auth_apis.py
@@ -1,12 +1,15 @@
 import pytest
-from app.schemas.user import UserRead
+from app.schemas.user import UserRead,LoginResponse
+
 
 @pytest.fixture
 def user_signup(client):
     def _user_signup(email="test@example.in", full_name="Test User", password="securepassword123!"):
-        response = client.post("/api/v1/auth/signup", json={"email": email, "full_name": full_name, "password": password})
+        response = client.post(
+            "/api/v1/auth/signup", json={"email": email, "full_name": full_name, "password": password})
         return response
     return _user_signup
+
 
 def test_user_signup_api(client, user_signup):
     response = user_signup(email="test1@example.in")
@@ -14,9 +17,20 @@ def test_user_signup_api(client, user_signup):
     assert UserRead.model_validate(response.json())
     assert response.json()["email"] == "test1@example.in"
 
+
 def test_should_not_allow_duplicate_email_signup(client, user_signup):
     response1 = user_signup(email="test@example.com")
     assert response1.status_code == 201
     response2 = user_signup(email="test@example.com")
     assert response2.status_code == 400
     assert response2.json()["detail"] == "Email already exists"
+
+
+def test_login_api_success(client, user_signup):
+    email = "test@example.com"
+    password = "securepassword123!"
+    user_signup(email=email, password=password)
+    response = client.post("/api/v1/auth/login", json={"email": email, "password": password})
+    assert response.status_code == 200
+    assert LoginResponse.model_validate(response.json())
+    assert response.json()["user"]["email"] == email
