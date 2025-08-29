9bb8abea0ada34501152e5e31aa0cd6aa3151730
Nitish Patel <nitish@incubyte.co>
2025-08-29 17:31:04 +0530

    test: added test cases for login service

--- task-manager-backend/tests/unit/auth/test_auth_service.py
+++ task-manager-backend/tests/unit/auth/test_auth_service.py
@@ -1,6 +1,6 @@
 import pytest
 from app.services.auth import AuthService
-from app.schemas.user import UserSignUpRequest,UserRead
+from app.schemas.user import UserSignUpRequest,UserRead,LoginResponse,UserLoginRequest
 
 @pytest.fixture
 def auth_service():
@@ -25,3 +25,41 @@ def test_duplicate_email_signup(auth_service: AuthService, db):
     with pytest.raises(ValueError) as exc_info:
         auth_service.signup(db, req)
     assert str(exc_info.value) == "Email already exists"
+
+def test_login_success(auth_service: AuthService, db):
+    email = "test@example.com"
+    password = "securepassword123!"
+    sign_up_req = UserSignUpRequest(
+        email=email,
+        password=password,
+    )
+    auth_service.signup(db, sign_up_req)
+    login_req = UserLoginRequest(
+        email=email,
+        password=password
+    )
+    response = auth_service.login(db, login_req)
+    assert isinstance(response, LoginResponse)
+
+def test_login_invalid_email(auth_service: AuthService, db):
+    login_req = UserLoginRequest(
+        email="user_not_exist@gmail.com",
+        password="somepassword"
+    )
+    response = auth_service.login(db, login_req)
+    assert response is None
+
+def test_invalid_password(auth_service: AuthService, db):
+    email = "test@example.in"
+    password = "securepassword123!"
+    sign_up_req = UserSignUpRequest(
+        email=email,
+        password=password,
+    )
+    auth_service.signup(db, sign_up_req)
+    login_req = UserLoginRequest(
+        email=email,
+        password="wrongpassword"
+    )
+    response = auth_service.login(db, login_req)
+    assert response is None
