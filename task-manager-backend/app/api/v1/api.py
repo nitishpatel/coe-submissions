5d3d31b3391d5aa3a16846a991a45013a7479792
Nitish Patel <nitish@incubyte.co>
2025-08-29 16:32:36 +0530

    chore: registered auth api router

--- task-manager-backend/app/api/v1/api.py
+++ task-manager-backend/app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
-from app.api.v1.endpoints import tasks
+from app.api.v1.endpoints import tasks,auth
 
 api_router = APIRouter(prefix="/v1")
 
 api_router.include_router(tasks.router, prefix="/tasks")
+api_router.include_router(auth.router, prefix="/auth")
