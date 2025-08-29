3b6eb8124a2e84798b917dea46c72afc4a84d33c
Nitish Patel <nitish@incubyte.co>
2025-08-29 14:04:02 +0530

    feat: added check for min one digit and special character

--- task-manager-backend/app/schemas/user.py
+++ task-manager-backend/app/schemas/user.py
@@ -1,6 +1,15 @@
-from pydantic import BaseModel,EmailStr,ConfigDict,Field
+from pydantic import BaseModel,EmailStr,ConfigDict,Field,field_validator
+import re
 
 class UserSignUpRequest(BaseModel):
     email: EmailStr
     full_name: str | None = None
     password: str = Field(...,min_length=8,max_length=26)
+
+    @field_validator('password')
+    def validate_password(cls, value: str) -> str:
+        if not re.search(r'[0-9]', value):
+            raise ValueError('Password must contain at least one digit')
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+            raise ValueError('Password must contain at least one special character')
+        return value
