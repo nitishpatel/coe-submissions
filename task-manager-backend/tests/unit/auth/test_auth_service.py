556f8bbd55341d1d1292830b28d0c9e64059674c
Nitish Patel <nitish@incubyte.co>
2025-08-29 16:45:01 +0530

    test: user service duplicate email signup test

--- task-manager-backend/tests/unit/auth/test_auth_service.py
+++ task-manager-backend/tests/unit/auth/test_auth_service.py
@@ -14,3 +14,14 @@ def test_signup(auth_service: AuthService, db):
     )
     user = auth_service.signup(db, req)
     assert isinstance(user, UserRead)
+
+def test_duplicate_email_signup(auth_service: AuthService, db):
+    req = UserSignUpRequest(
+        email="test@example.com",
+        full_name="Test User",
+        password="securepassword123!"
+    )
+    auth_service.signup(db, req)
+    with pytest.raises(ValueError) as exc_info:
+        auth_service.signup(db, req)
+    assert str(exc_info.value) == "Email already exists"
