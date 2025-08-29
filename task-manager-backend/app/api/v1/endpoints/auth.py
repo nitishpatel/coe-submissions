11403a46875a2581ddc8ebb38b1a7d92e64e027c
Nitish Patel <nitish@incubyte.co>
2025-08-29 17:45:32 +0530

    feat: added auth/login endpoint

--- task-manager-backend/app/api/v1/endpoints/auth.py
+++ task-manager-backend/app/api/v1/endpoints/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.services.auth import AuthService
 from app.api.deps import DB
-from app.schemas.user import UserSignUpRequest, UserRead
+from app.schemas.user import UserSignUpRequest, UserRead, UserLoginRequest, LoginResponse
 
 router = APIRouter(tags=["auth"])
 
@@ -11,3 +11,10 @@ def signup(payload: UserSignUpRequest, db: DB):
         return AuthService().signup(db, payload)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@router.post("/login", response_model=LoginResponse)
+def login(payload: UserLoginRequest, db: DB):
+    login_reponse = AuthService().login(db, payload)
+    if not login_reponse:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    return login_reponse
