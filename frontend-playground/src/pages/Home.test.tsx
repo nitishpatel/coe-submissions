b43439572fd206e5fd7172dc727e966e1004bbf6
Nitish Patel <nitish@incubyte.co>
2025-09-02 12:41:58 +0530

    test: test for increment button to be in dom

--- frontend-playground/src/pages/Home.test.tsx
+++ frontend-playground/src/pages/Home.test.tsx
@@ -4,6 +4,11 @@ import Home from "./Home";
 describe("Home component", () => {
   it("renders the home page heading", () => {
     render(<Home />);
-    expect(screen.getByText(/home/i)).toBeInTheDocument();
+    expect(screen.getByText(/Counter Home Page/i)).toBeInTheDocument();
   });
+
+  it("should have an increment button",()=>{
+    render(<Home/>);
+    expect(screen.getByRole("button",{name:/increment/i})).toBeInTheDocument();
+  })
 });
