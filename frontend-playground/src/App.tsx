66cad10f5a0d33c07dc68f201c604cf74994bc5d
Nitish Patel <nitish@incubyte.co>
2025-09-02 15:51:10 +0530

    changed routing

--- frontend-playground/src/App.tsx
+++ frontend-playground/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/Home';
+import Hero from "./pages/Hero";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/counter' element={<Home/>}/>
+        <Route path='/' element={<Hero/>}/>
       </Routes>
     </BrowserRouter>
   )
