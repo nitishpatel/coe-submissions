ceb0d5727e0a237a071cf603e33359167c5d84f3
Nitish Patel <nitish@incubyte.co>
2025-09-02 17:27:04 +0530

    updated ui with tailwind classes

--- frontend-playground/src/pages/Home.tsx
+++ frontend-playground/src/pages/Home.tsx
@@ -7,16 +7,25 @@ const Home = () => {
   });
 
   const handleIncrement = () => {
-    const updatedCounter:number = counter + 1;
+    const updatedCounter: number = counter + 1;
     setCounter(updatedCounter);
-    localStorage.setItem("counter",updatedCounter.toString());
+    localStorage.setItem("counter", updatedCounter.toString());
   };
 
   return (
-    <div>
-      <h1>Counter Home Page</h1>
-      <h2>Counter : {counter}</h2>
-      <button onClick={handleIncrement}>increment</button>
+    <div className="flex-1 flex flex-col justify-center items-center px-4">
+      <h5 className="text-4xl font-medium max-w-[850px] text-center">
+        So what you counting?
+      </h5>
+      <h5 className="text-4xl md:text-7xl font-medium max-w-[850px] text-center my-20">
+        Counter : {counter}
+      </h5>
+      <button
+        className="bg-slate-800 hover:bg-black text-white px-6 py-3 rounded-full font-medium transition"
+        onClick={handleIncrement}
+      >
+        Increment
+      </button>
     </div>
   );
 };
