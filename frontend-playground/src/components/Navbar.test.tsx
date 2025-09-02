377c15031d530ce32d7e0bdea4723db507a439ed
Nitish Patel <nitish@incubyte.co>
2025-09-02 18:02:08 +0530

    test: should  redirect to index

---
+++ frontend-playground/src/components/Navbar.test.tsx
@@ -0,0 +1,23 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { createMemoryHistory, type MemoryHistory } from "history";
+import Hero from "../pages/Hero";
+import { Router } from "react-router";
+import Navbar from "./Navbar";
+
+describe("Navbar", () => {
+  let history: MemoryHistory
+  beforeEach(()=>{
+    localStorage.clear();
+    render(<Navbar/>);
+    history = createMemoryHistory({ initialEntries: ["/counter"] });
+  });
+  it("should have the project title",()=>{
+    expect(screen.getByText(/CountPlusPlus/i)).toBeVisible();
+  });
+
+  it("should navigate to / on click of Navbar Button",()=>{
+    expect(history.location.pathname).toBe("/counter");
+    fireEvent.click(screen.getByText(/CountPlusPlus/i));
+    expect(history.location.pathname).toBe("/");
+  });
+});
