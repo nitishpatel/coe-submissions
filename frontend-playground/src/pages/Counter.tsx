160b1b21128ff9fb0cb4b11a1c4dc38d53071b53
Nitish Patel <nitish@incubyte.co>
2025-09-02 17:38:06 +0530

    feat: added reset button

--- frontend-playground/src/pages/Counter.tsx
+++ frontend-playground/src/pages/Counter.tsx
@@ -20,12 +20,20 @@ const Counter = () => {
       <h5 className="text-4xl md:text-7xl font-medium max-w-[850px] text-center my-20">
         Counter : {counter}
       </h5>
-      <button
-        className="bg-slate-800 hover:bg-black text-white px-6 py-3 rounded-full font-medium transition"
-        onClick={handleIncrement}
-      >
-        Increment
-      </button>
+      <div className="flex flex-row space-x-1">
+        <button
+          className="bg-slate-800 hover:bg-black text-white px-6 py-3 rounded-full font-medium transition"
+          onClick={handleIncrement}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-orange-800 hover:bg-orange text-white px-6 py-3 rounded-full font-medium transition"
+          onClick={handleIncrement}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
