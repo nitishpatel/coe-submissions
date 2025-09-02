58a699cd89cbde72ad2bda6d6f6e8c8bbd0beea9
Nitish Patel <nitish@incubyte.co>
2025-09-02 12:47:33 +0530

    feat: introduced counter state and counter text

--- frontend-playground/src/pages/Home.tsx
+++ frontend-playground/src/pages/Home.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [counter,setCounter] = useState<number>(0);
   return (
     <div>
       <h1>Counter Home Page</h1>
+      <h2>Counter : {counter}</h2>
       <button>increment</button>
     </div>
   )
