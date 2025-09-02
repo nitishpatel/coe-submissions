6ee918b3306ab7d66766eb5617f3d7aab995c817
Nitish Patel <nitish@incubyte.co>
2025-09-02 13:08:42 +0530

    feat: added counter state and increment button handler

--- frontend-playground/src/pages/Home.tsx
+++ frontend-playground/src/pages/Home.tsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 
 const Home = () => {
   const [counter,setCounter] = useState<number>(0);
+
+  const handleIncrement = () => {
+    setCounter(counter+1);
+  };
   return (
     <div>
       <h1>Counter Home Page</h1>
       <h2>Counter : {counter}</h2>
-      <button>increment</button>
+      <button onClick={handleIncrement}>increment</button>
     </div>
   )
 }
