51021691cc0c58ac8d7f6fe9c707aba0e9e2c50a
Nitish Patel <nitish@incubyte.co>
2025-09-02 12:22:33 +0530

    feat: introduced home page route via react router

--- frontend-playground/src/App.tsx
+++ frontend-playground/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './pages/home';
+import Home from './pages/Home';
 
 const App = () => {
   return (
