eb8798fbafcd066a42f2eb42e52eeb4b6de02b96
Nitish Patel <nitish@incubyte.co>
2025-09-03 13:49:52 +0530

    feat: signup page form added

--- task-manager-frontend/src/pages/auth/Signup.tsx
+++ task-manager-frontend/src/pages/auth/Signup.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Register</div>
+    <div>
+      <div>
+        <h1>Register</h1>
+      </div>
+      <form aria-label='signup-form'>
+
+      </form>
+    </div>
   )
 }
 
