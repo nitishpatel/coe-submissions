b5706f7efcf05d7819ccaa8d9ee08046ee656fef
Nitish Patel <nitish@incubyte.co>
2025-09-03 15:56:52 +0530

    refactor: checking for extact error messaged instead of just boolean schema parsing checks

--- task-manager-frontend/src/schemas/SignupSchema.test.ts
+++ task-manager-frontend/src/schemas/SignupSchema.test.ts
@@ -1,27 +1,59 @@
 import { signupSchema } from "./SignupSchema";
 
-describe("Signup schema",()=>{
-  it("fails if email is missing",()=>{
-    const result = signupSchema.safeParse({});
-    expect(result.success).toBe(false);
-  })
-  it("should not fail if full name is missing",()=>{
+const validBaseUser = {
+  email: "test@example.in",
+  password: "Test@123",
+};
+
+describe("Signup schema", () => {
+  it("fails if email is missing", () => {
     const result = signupSchema.safeParse({
-      email:"test@example.in"
+      password: validBaseUser.password,
     });
+
+    expect(result.success).toBe(false);
+
+    if (!result.success) {
+      expect(result.error.issues).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: ["email"],
+            message: "Invalid email",
+          }),
+        ])
+      );
+    }
+  });
+
+  it("fails if password is missing", () => {
+    const result = signupSchema.safeParse({
+      email: validBaseUser.email,
+    });
+
+    expect(result.success).toBe(false);
+
+    if (!result.success) {
+      expect(result.error.issues).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: ["password"],
+            message: "Password is mandatory",
+          }),
+        ])
+      );
+    }
+  });
+
+  it("should not fail if full name is missing", () => {
+    const result = signupSchema.safeParse(validBaseUser);
     expect(result.success).toBe(true);
   });
-  it("allows an fullname",()=>{
+
+  it("allows a full name", () => {
     const result = signupSchema.safeParse({
-      email:"test@example.in",
-      fullname:"Test User"
+      ...validBaseUser,
+      fullName: "Test User",
     });
     expect(result.success).toBe(true);
-  });
-  it("fails if password is missing",()=>{
-    const result = signupSchema.safeParse({
-      email:"test@example.in"
-    });
-    expect(result.success).toBe(false);
   });
 });
