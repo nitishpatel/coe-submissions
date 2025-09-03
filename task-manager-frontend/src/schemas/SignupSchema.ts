dc5466c5af32cbaf5ef28ad03d8e5bb3016fb6c4
Nitish Patel <nitish@incubyte.co>
2025-09-03 15:42:38 +0530

    feat: signup schema optinal full name

--- task-manager-frontend/src/schemas/SignupSchema.ts
+++ task-manager-frontend/src/schemas/SignupSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.email("Invalid email"),
+    fullname:z.string().optional().nullable()
   }).strict();
 
 export type SignupFormData = z.infer<typeof signupSchema>;
