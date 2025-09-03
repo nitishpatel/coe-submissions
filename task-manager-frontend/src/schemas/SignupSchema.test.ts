691bc63e313d1278bd8553dbc9cb204ffb9a31fc
Nitish Patel <nitish@incubyte.co>
2025-09-03 15:40:43 +0530

    test: signup schema allows an fullname

--- task-manager-frontend/src/schemas/SignupSchema.test.ts
+++ task-manager-frontend/src/schemas/SignupSchema.test.ts
@@ -1,8 +1,21 @@
-import { signupSchema } from "./SignupSchemas";
+import { signupSchema } from "./SignupSchema";
 
 describe("Signup schema",()=>{
   it("fails if email is missing",()=>{
     const result = signupSchema.safeParse({});
     expect(result.success).toBe(false);
   })
+  it("should not fail if full name is missing",()=>{
+    const result = signupSchema.safeParse({
+      email:"test@example.in"
+    });
+    expect(result.success).toBe(true);
+  });
+  it("allows an fullname",()=>{
+    const result = signupSchema.safeParse({
+      email:"test@example.in",
+      fullname:"Test User"
+    });
+    expect(result.success).toBe(true);
+  });
 });
--- task-manager-frontend/src/schemas/SignupSchema.ts
+++ task-manager-frontend/src/schemas/SignupSchema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.email("Invalid email"),
-  })
+  }).strict();
 
 export type SignupFormData = z.infer<typeof signupSchema>;
