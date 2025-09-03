3bbc85de11a24aac940c4a8561308f15035cb899
Nitish Patel <nitish@incubyte.co>
2025-09-03 13:49:41 +0530

    test: signup page should have a form

--- task-manager-frontend/src/pages/auth/Signup.test.tsx
+++ task-manager-frontend/src/pages/auth/Signup.test.tsx
@@ -8,4 +8,9 @@ describe("Signup Page", () => {
   it("renders the signup page heading", () => {
     expect(screen.getByText(/Register/i)).toBeInTheDocument();
   });
+  it("should have an form",()=>{
+    expect(screen.getByRole("form",{
+      name:"signup-form"
+    }))
+  })
 });
