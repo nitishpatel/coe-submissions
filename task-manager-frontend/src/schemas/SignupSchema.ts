6cb03d53b2b92c6d37a5220b42d78951b8020b6d
Nitish Patel <nitish@incubyte.co>
2025-09-03 16:07:16 +0530

    feat: signup schema password min length to be 8

--- task-manager-frontend/src/schemas/SignupSchema.ts
+++ task-manager-frontend/src/schemas/SignupSchema.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.email("Invalid email"),
-    fullName:z.string().optional().nullable(),
-    password:z.string("Password is mandatory"),
-    confirmPassword:z.string("Confirm Password is mandatory")
+    fullName: z.string().optional().nullable(),
+    password: z
+    .string("Password is mandatory")
+    .min(8, "Password must be between 8 and 26 characters"),
+    confirmPassword: z.string("Confirm Password is mandatory")
   }).strict();
 
 export type SignupFormData = z.infer<typeof signupSchema>;
