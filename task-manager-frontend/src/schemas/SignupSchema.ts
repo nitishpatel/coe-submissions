aad84464d8a9d2b085897455b3a95b4d6b79ab4c
Nitish Patel <nitish@incubyte.co>
2025-09-03 15:59:24 +0530

    fix: changed schema fullname to fullName for naming convention

--- task-manager-frontend/src/schemas/SignupSchema.ts
+++ task-manager-frontend/src/schemas/SignupSchema.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.email("Invalid email"),
-    fullname:z.string().optional().nullable()
+    fullName:z.string().optional().nullable(),
+    password:z.string("Password is mandatory")
   }).strict();
 
 export type SignupFormData = z.infer<typeof signupSchema>;
