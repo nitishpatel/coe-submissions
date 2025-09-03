9f3ac9934187eaf2d1d4b0bc32e969db5f6da089
Nitish Patel <nitish@incubyte.co>
2025-09-03 17:56:43 +0530

    fix: added types to authservice.register

--- task-manager-frontend/src/services/authService.ts
+++ task-manager-frontend/src/services/authService.ts
@@ -1,9 +1,10 @@
 import type { SignupFormData } from "../schemas/SignupSchema";
+import type { User } from "../types";
 import { httpService } from "./http";
 
 export const authService = {
   register: async (data: SignupFormData) => {
-    const response = await httpService.post("/auth/signup", data);
+    const response = await httpService.post<User>("/auth/signup", data);
     return response.data;
   }
 };
