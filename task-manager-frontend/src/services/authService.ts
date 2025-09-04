1f4b56114c7e69245db6b653b8f48c39f9aacd91
Nitish Patel <nitish@incubyte.co>
2025-09-04 11:02:01 +0530

    feat: implemented login api call in auth service

--- task-manager-frontend/src/services/authService.ts
+++ task-manager-frontend/src/services/authService.ts
@@ -1,10 +1,15 @@
+import type { LoginFormData } from "../schemas/LoginSchema";
 import type { SignupFormData } from "../schemas/SignupSchema";
-import type { User } from "../types";
+import type { LoginResponse, User } from "../types";
 import { httpService } from "./http";
 
 export const authService = {
   register: async (data: SignupFormData) => {
     const response = await httpService.post<User>("/auth/signup", data);
+    return response.data;
+  },
+  login: async (data: LoginFormData) => {
+    const response = await httpService.post<LoginResponse>("/auth/login", data);
     return response.data;
   }
 };
