b66d4846b096892c05066417260f9e122bbd7dc1
Nitish Patel <nitish@incubyte.co>
2025-09-04 09:37:30 +0530

    test: login schema fails if email is invalid

--- task-manager-frontend/src/schemas/LoginSchema.test.ts
+++ task-manager-frontend/src/schemas/LoginSchema.test.ts
@@ -1,3 +1,4 @@
+import { email } from "zod";
 import { loginSchema } from "./LoginSchema";
 
 const validBaseUser = {
@@ -8,6 +9,25 @@ const validBaseUser = {
 describe("Login schema", () => {
   it("fails if email is missing", () => {
     const result = loginSchema.safeParse({
+      password: validBaseUser.password,
+    });
+
+    expect(result.success).toBe(false);
+
+    if (!result.success) {
+      expect(result.error.issues).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: ["email"],
+            message: "Invalid email",
+          }),
+        ])
+      );
+    }
+  });
+  it("fails if email is invalid", () => {
+    const result = loginSchema.safeParse({
+      email:"test",
       password: validBaseUser.password,
     });
 
