9eee38aad5c55efb0a1ddc9258963d07c4dad278
Nitish Patel <nitish@incubyte.co>
2025-09-04 11:06:01 +0530

    test: login form > login button on click should trigger auth/login api call

--- task-manager-frontend/src/pages/auth/Login.test.tsx
+++ task-manager-frontend/src/pages/auth/Login.test.tsx
@@ -1,7 +1,12 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Login from "./Login";
+import { authService } from "../../services/authService";
 
-
+vi.mock("../../services/authService", () => ({
+  authService: {
+    login: vi.fn(),
+  },
+}));
 
 describe("Login Page", () => {
   let unmountlogin: () => void;
@@ -42,6 +47,43 @@ describe("Login Page", () => {
       expect(
         await screen.findByText(/Please enter a password/i)
       ).toBeInTheDocument();
+    });
+  });
+  describe("Signup form integration", () => {
+    it("calls authService.register with form data", async () => {
+      unmountlogin();
+      const mockLogin = vi.mocked(authService.login);
+      mockRegister.mockResolvedValueOnce({
+        access_token:
+          "dummy_jwt_token",
+        token_type: "bearer",
+        user: {
+          id: "2bab6bc1-b938-4867-99f4-3761db08d8b9",
+          email: "test@example.com",
+          full_name: null,
+          is_active: true,
+          created_at: "2025-08-29T11:11:49.382281",
+          updated_at: "2025-08-29T11:11:49.382281",
+        },
+      });
+      render(<Login />);
+      fireEvent.input(screen.getByPlaceholderText(/email/i), {
+        target: { value: "test@example.in" },
+      });
+      fireEvent.input(screen.getByLabelText(/^password$/i), {
+        target: { value: "Test@123" },
+      });
+
+      await fireEvent.click(screen.getByRole("button", { name: /login/i }));
+
+      await waitFor(() => {
+        expect(mockLogin).toHaveBeenCalledWith(
+          expect.objectContaining({
+            email: "test@example.in",
+            password: "Test@123",
+          })
+        );
+      });
     });
   });
 });
