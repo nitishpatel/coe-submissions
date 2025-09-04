593edc950f01f7e8d9d0081f6a4642c550002f78
Nitish Patel <nitish@incubyte.co>
2025-09-04 17:15:27 +0530

    test: task delete modal title,buttons to be visible

---
+++ task-manager-frontend/src/components/TaskDeleteModal.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import TaskDeleteModal from "./TaskDeleteModal";
+
+describe("Task Delete Modal", () => {
+  beforeEach(() => {
+    render(
+      <TaskDeleteModal
+        onClose={() => {}}
+        title="TaskA"
+      />
+    );
+  });
+  it("renders the title of the task to be delete in the modal", () => {
+    expect(screen.getByText(/TaskA/i)).toBeInTheDocument();
+  });
+  it("renders the cancel button",()=>{
+    expect(screen.getByRole("button",{name:/cancel/i})).toBeVisible();
+  })
+  it("renders the save button",()=>{
+    expect(screen.getByRole("button",{name:/delete/i})).toBeVisible();
+  })
+});
