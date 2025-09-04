9df3caa3078220cd8cef7624ad5742050cb2a15e
Nitish Patel <nitish@incubyte.co>
2025-09-04 17:27:34 +0530

    feat: taskservice implemented with tasklist api

---
+++ task-manager-frontend/src/services/taskService.ts
@@ -0,0 +1,10 @@
+
+import type { TaskList } from "../types";
+import { httpService } from "./http";
+
+export const taskService = {
+  list: async () => {
+    const {data} = await httpService.get<TaskList>("/tasks");
+    return data;
+  }
+};
