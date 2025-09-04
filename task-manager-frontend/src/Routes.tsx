1772cb7d6ee6751a4e798d862e6338fbb7087705
Nitish Patel <nitish@incubyte.co>
2025-09-04 10:55:01 +0530

    chore: registered login page in routes

--- task-manager-frontend/src/Routes.tsx
+++ task-manager-frontend/src/Routes.tsx
@@ -1,9 +1,14 @@
 import type { RouteObject } from "react-router";
 import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
 
 export const routes:RouteObject[] = [
   {
     "path":"/register",
     "element":<Signup/>
+  },
+  {
+    "path":"/login",
+    element:<Login/>
   }
 ];
