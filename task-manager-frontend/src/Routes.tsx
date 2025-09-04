43e2ff4896438a384d02140317024b098f567832
Nitish Patel <nitish@incubyte.co>
2025-09-04 15:30:45 +0530

    chore: added authenticated and guest routes wrapper

--- task-manager-frontend/src/Routes.tsx
+++ task-manager-frontend/src/Routes.tsx
@@ -2,18 +2,32 @@ import type { RouteObject } from "react-router";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import TaskList from "./pages/task/TaskList";
+import UnprotectedRoute from "./routes/UnprotectedRoute";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
-export const routes:RouteObject[] = [
+export const routes: RouteObject[] = [
   {
-    path:"/register",
-    element:<Signup/>
+    path: "/",
+    element: <UnprotectedRoute />,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
   {
-    path:"/login",
-    element:<Login/>
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/task-list",
+        element: <TaskList />,
+      },
+    ],
   },
-  {
-    path:"/task-list",
-    element:<TaskList/>
-  }
 ];
