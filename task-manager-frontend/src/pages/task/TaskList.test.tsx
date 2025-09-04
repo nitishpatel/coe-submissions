fd9413d5ea6f8db587de2f8cd153a289c2d3e382
Nitish Patel <nitish@incubyte.co>
2025-09-04 17:54:02 +0530

    test: task list page should render titles, filter dropdowns, status columns, buttons

--- task-manager-frontend/src/pages/task/TaskList.test.tsx
+++ task-manager-frontend/src/pages/task/TaskList.test.tsx
@@ -1,11 +1,53 @@
 import { render, screen } from "@testing-library/react";
 import TaskList from "./TaskList";
+import { Router } from "react-router";
+import { createMemoryHistory, type MemoryHistory } from "history";
+import * as RRD from "react-router";
+import { taskListResponseMock } from "../../mocks/taskResponse.mock";
 
+vi.mock("react-router", async () => {
+  const actual: typeof RRD = await vi.importActual("react-router");
+  return {
+    ...actual,
+    useLoaderData: vi.fn(),
+  };
+});
 describe("Tasklist", () => {
+  let history:MemoryHistory;
   beforeEach(() => {
-    render(<TaskList/>)
+
+    history = createMemoryHistory({
+      initialEntries: ["/login"],
+    });
+    (RRD.useLoaderData as unknown as vi.Mock).mockReturnValue(taskListResponseMock);
+    render(<Router location={history.location.pathname} navigator={history}>
+      <TaskList/>
+    </Router>)
   });
   it("renders the heading of the tasklist page", () => {
     expect(screen.getByText(/Your Tasks/i)).toBeInTheDocument();
   });
+  it("renders the one column for each task status", () => {
+    expect(screen.getByText(/Queue/i)).toBeInTheDocument();
+    expect(screen.getByText(/WIP/i)).toBeInTheDocument();
+    expect(screen.getByText(/Completed/i)).toBeInTheDocument();
+  });
+  it("renders the add task button", () => {
+    expect(screen.getByRole("button",{name:/Add Task/i})).toBeInTheDocument();
+  });
+  it("renders the reset button", () => {
+    expect(screen.getByRole("button",{name:/reset/i})).toBeInTheDocument();
+  });
+  it("renders the pagination button", () => {
+    expect(screen.getByRole("button",{name:/prev/i})).toBeInTheDocument();
+    expect(screen.getByRole("button",{name:/next/i})).toBeInTheDocument();
+  });
+  it("renders the filter dropdowns", () => {
+    expect(screen.getByLabelText(/status/i  )).toBeInTheDocument();
+    expect(screen.getByLabelText(/created from/i  )).toBeInTheDocument();
+    expect(screen.getByLabelText(/created to/i  )).toBeInTheDocument();
+    expect(screen.getByLabelText(/sort by/i  )).toBeInTheDocument();
+    expect(screen.getByLabelText(/order/i  )).toBeInTheDocument();
+  });
+
 });
