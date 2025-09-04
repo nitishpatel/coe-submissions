f55059a8ea7c47fe36fde48761d31918d27fcc88
Nitish Patel <nitish@incubyte.co>
2025-09-04 14:38:07 +0530

    feat: implemented loginSuccess method in authstore

--- task-manager-frontend/src/store/authStore.ts
+++ task-manager-frontend/src/store/authStore.ts
@@ -1,25 +1,35 @@
-import {create} from "zustand";
-import type { User } from "../types";
+import { create } from "zustand";
+import type { LoginResponse, User } from "../types";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 type AuthStore = {
   user: User | null;
-  token: string|null;
-  isAuthenticated:boolean;
+  token: string | null;
+  isAuthenticated: boolean;
 }
 
-type Store = AuthStore
+type AuthActions = {
+  loginSuccess: (loginResponse: LoginResponse) => void
+}
+type Store = AuthStore & AuthActions
 
 export const useAuthStore = create<Store>()(
   persist<Store>(
-    (set,get):Store=>({
-      user:null,
-      token:null,
-      isAuthenticated:false
+    (set, get): Store => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+      loginSuccess: (loginResponse) => {
+        set(() => ({
+          user: loginResponse.user,
+          token: loginResponse.access_token,
+          isAuthenticated: true
+        }))
+      },
     }),
     {
-      name:"auth-store",
-      storage:createJSONStorage(()=>localStorage)
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage)
     }
   )
 );
