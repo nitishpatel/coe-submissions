447d62e6741037fa66a11ab73f48de5e37be0be4
Nitish Patel <nitish@incubyte.co>
2025-09-04 09:28:13 +0530

    feat: added login page

---
+++ task-manager-frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col justify-center sm:h-screen p-4">
+      <div className="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8">
+        <div className="text-center mb-12">
+          <a href="javascript:void(0)">Login to TaskPlusPlus</a>
+        </div>
+
+        <form aria-label="login-form">
+          <div className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="text-slate-900 text-sm font-medium mb-2 block"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter email"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-slate-900 text-sm font-medium mb-2 block"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="text-slate-900 bg-white border border-gray-300 w-full text-sm px-4 py-3 rounded-md outline-blue-500"
+                placeholder="Enter password"
+              />
+            </div>
+          </div>
+
+          <div className="mt-12">
+            <button
+              type="submit"
+              className="w-full py-3 px-4 text-sm tracking-wider font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
