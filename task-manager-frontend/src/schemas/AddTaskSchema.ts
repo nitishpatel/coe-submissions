a346a7f340aa337ad50e8e10cd339eeb2fe89216
Nitish Patel <nitish@incubyte.co>
2025-09-08 12:01:11 +0530

    feat: addtask schema validation for title to be between 2 to 200 characters long

--- task-manager-frontend/src/schemas/AddTaskSchema.ts
+++ task-manager-frontend/src/schemas/AddTaskSchema.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const addTaskSchema = z
   .object({
-    title: z.string("Title should be between 2 to 200 characters long"),
+    title: z.string("Title should be between 2 to 200 characters long")
+    .min(2,"Title should be between 2 to 200 characters long")
+    .max(200,"Title should be between 2 to 200 characters long"),
   })
   .strict()
 
