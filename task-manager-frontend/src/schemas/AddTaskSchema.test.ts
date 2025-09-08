ae71f87fa7435c0ee5335bb81205ae5edce61db4
Nitish Patel <nitish@incubyte.co>
2025-09-08 11:59:12 +0530

    test: addTask schema title should be of length 2 to 200

--- task-manager-frontend/src/schemas/AddTaskSchema.test.ts
+++ task-manager-frontend/src/schemas/AddTaskSchema.test.ts
@@ -25,4 +25,40 @@ describe("AddTask schema", () => {
       );
     }
   });
+  it("fails if title is less than 2 characters", () => {
+    const result = addTaskSchema.safeParse({
+      title:"A"
+    });
+
+    expect(result.success).toBe(false);
+
+    if (!result.success) {
+      expect(result.error.issues).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: ["title"],
+            message: "Title should be between 2 to 200 characters long",
+          }),
+        ])
+      );
+    }
+  });
+  it("fails if title is more than 200 characters", () => {
+    const result = addTaskSchema.safeParse({
+      title:"A".repeat(201)
+    });
+
+    expect(result.success).toBe(false);
+
+    if (!result.success) {
+      expect(result.error.issues).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            path: ["title"],
+            message: "Title should be between 2 to 200 characters long",
+          }),
+        ])
+      );
+    }
+  });
 });
