fd47bca1cbcaecb9f2a8fe21f6658e3c365a8f03
Nitish Patel <nitish@incubyte.co>
2025-09-08 12:12:05 +0530

    feat: added descrption field to addTask schema

--- task-manager-frontend/src/schemas/AddTaskSchema.ts
+++ task-manager-frontend/src/schemas/AddTaskSchema.ts
@@ -5,6 +5,7 @@ export const addTaskSchema = z
     title: z.string("Title should be between 2 to 200 characters long")
     .min(2,"Title should be between 2 to 200 characters long")
     .max(200,"Title should be between 2 to 200 characters long"),
+    description: z.string().nullable().optional()
   })
   .strict()
 
